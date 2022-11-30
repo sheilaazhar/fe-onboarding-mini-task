d660cdfe6c8f113a166b1d7272f2017012682927
sheilaazhar <sheila.azhar98@gmail.com>
2022-11-30 11:07:21 +0700

    create main layout

---
+++ src/components/Layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="max-w-lg min-h-screen mx-auto shadow-md">
+    <div class="bg-[#FEFEFE]">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseLayout',
+};
+</script>
