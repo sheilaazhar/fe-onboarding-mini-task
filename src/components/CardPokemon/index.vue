8c2792cad2f8d303ed236296c2d69bee5d09dea4
sheilaazhar <sheila.azhar98@gmail.com>
2022-12-12 09:51:20 +0700

    card bg-color based on their type

--- src/components/CardPokemon/index.vue
+++ src/components/CardPokemon/index.vue
@@ -1,18 +1,27 @@
 <template>
-  <article class="relative pt-5 pb-3 rounded-lg shadow flex flex-col items-center transition ease ring-2 ring-transparent hover:ring-emerald-500 hover:ring-offset-2 bg-green-700 bg-opacity-20">
-    <div class="absolute leading-none top-[2px] left-[2px] rounded-tl-lg rounded py-1 px-2 flex items-center bg-green-500 bg-opacity-60">
-      <span class="text-xs text-black">#{{ pokemon.id }}</span>
+  <article :class="('relative pt-5 pb-3 rounded-lg shadow flex flex-col items-center transition ease ring-2 ring-transparent hover:ring-emerald-500 hover:ring-offset-2 bg-opacity-20 bg-'+types[0].type.name)">
+    <div class="absolute leading-none top-[2px] left-[2px] rounded-tl-lg rounded py-1 px-2 flex items-center bg-gray-100 bg-opacity-60">
+      <span class="text-xs text-black">#{{ id }}</span>
     </div>
     <img
-      :src="imageUrl + pokemon.id + '.png'"
-      :alt="pokemon.name + ' image'"
+      :src="imageUrl + id + '.png'"
+      :alt="name + ' image'"
       height="140"
       width="140"
       class="h-auto w-36"
     >
     <h2 class="mt-2 text-center font-semibold capitalize">
-      {{ pokemon.name }}
+      {{ name }}
     </h2>
+    <ul class="flex items-center space-x-1">
+      <li
+        v-for="(value, index) in types"
+        :key="'value'+index"
+        :class="value.type.name + ' text-white rounded py-[2px] capitalize text-xs px-2'"
+      >
+        {{ value.type.name }}
+      </li>
+    </ul>
   </article>
 </template>
 
@@ -20,7 +29,15 @@
 export default {
   name: 'CardPokemon',
   props: {
-    pokemon: {
+    id: {
+      type: [Number, String],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    types: {
       type: Array,
       required: true,
     },
