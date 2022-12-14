be213e98a052f3bb834dcfe34bf54529327e39a4
sheilaazhar <sheila.azhar98@gmail.com>
2022-12-14 12:13:07 +0700

    add loader

---
+++ src/components/Loader/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex items-center justify-center space-x-2 animate-bounce"
+  >
+    <div class="w-8 h-8 bg-blue-400 rounded-full" />
+    <div class="w-8 h-8 bg-green-400 rounded-full" />
+    <div class="w-8 h-8 bg-black rounded-full" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoaderIcon',
+};
+</script>
